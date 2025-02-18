2ad3cb519381c612330279e4623c45666d746a24
zifeng175mo <378663429@qq.com>
2025-02-18 07:46:21 +0000

    Add judge_ret for sail_test.

--- sail_test.sh
+++ sail_test.sh
@@ -1,4 +1,16 @@
+function judge_ret() {
+  if [[ $1 == 0 ]]; then
+    echo -e "\033[32m Passed: $2 \033[0m"
+    echo ""
+  else
+    echo -e "\033[31m Failed: $2 \033[0m"
+    exit 1
+  fi
+  sleep 2
+}
+
 pushd sophon-sail2/tests
+
 if [! -d "./data"];
 then
     mkdir ./data
@@ -21,30 +33,37 @@ fi
 
 pushd ./Decoder
 python3 -m pytest test_Decoder.py
+judge_ret $? "test_Decoder of sail_test"
 popd
 
 pushd ./Encoder
 python3 -m pytest test_Encoder.py
+judge_ret $? "test_Encoder of sail_test"
 popd
 
 pushd ./Engine
 python3 -m pytest test_Engine.py
+judge_ret $? "test_Engine of sail_test"
 popd
 
 pushd ./Functional
 python3 -m pytest test_Functional.py
+judge_ret $? "test_Functional of sail_test"
 popd
 
 pushd ./Image
 python3 -m pytest test_Image.py
+judge_ret $? "test_Image of sail_test"
 popd
 
 pushd ./Stream
 python3 -m pytest test_Stream.py
+judge_ret $? "test_Stream of sail_test"
 popd
 
 pushd ./Tensor
 pythone -m pytest test_Tensor.py
+judge_ret $? "test_Tensor of sail_test"
 popd
 
 popd
